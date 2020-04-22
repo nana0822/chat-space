b5cebdf0aaac6f6e2a3513534cefaf23a082394a
nana0822 <63431900+nana0822@users.noreply.github.com>
2020-04-22 11:51:12 +0900

    Indent

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -40,26 +40,26 @@ $(function(){
    };
  }
  $('#new_message').on('submit', function(e){
-  e.preventDefault();
-  var formData = new FormData(this);
-  var url = $(this).attr('action')
-  $.ajax({
-    url: url,
-    type: "POST",
-    data: formData,
-    dataType: 'json',
-    processData: false,
-    contentType: false
+ e.preventDefault();
+ var formData = new FormData(this);
+ var url = $(this).attr('action')
+ $.ajax({
+  url: url,
+  type: "POST",
+  data: formData,
+  dataType: 'json',
+  processData: false,
+  contentType: false
+ })
+  .done(function(data){
+    var html = buildHTML(data);
+    $('.messages').append(html);
+    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
+    $('form')[0].reset();
   })
-    .done(function(data){
-      var html = buildHTML(data);
-      $('.messages').append(html);
-      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');   
-      $('form')[0].reset();
-    })
-     .fail(function() {
-       alert("メッセージ送信に失敗しました");
-      });
-      return false;
-      })
+   .fail(function() {
+     alert("メッセージ送信に失敗しました");
+   });
+   return false;
+ })
 });
